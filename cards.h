887807e7989bdc4070763c23f56d5100a748f58f
CrazyMustard <simaosge6@hotmail.com>
2016-05-12 16:47:49 +0100

    Stack elements changed to Card_node

--- cards.h
+++ cards.h
@@ -20,12 +20,6 @@ typedef struct card_node {
 	struct card_node* next;
 } Card_node;
 
-typedef struct cardstack_node{
-	Card card;
-	struct cardstack_node* prev;
-} CardStack_node;
-
-
 stat add_card(Card_node** head, Card data);
 stat insert_card(Card_node** head,Card data, int position);
 Card_node* take_node_card(Card_node** head, int position);
@@ -33,9 +27,9 @@ stat join_node_card(Card_node** head, Card_node* jointo, int position);
 Card_node* create_node_card(Card data);
 void erase_card_list(Card_node* head);
 
-void push_card(CardStack_node** head, Card data);
-Card pop_card(CardStack_node** head);
-Card card_stack_top(CardStack_node* head);
-void card_stack_erase(CardStack_node** head);
+void push_card(Card_node** head, Card data);
+Card pop_card(Card_node** head);
+Card card_stack_top(Card_node* head);
+void card_stack_erase(Card_node** head);
 
 #endif
