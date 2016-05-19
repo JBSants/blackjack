6c0edeb97eab00904e0d3f583a8726e6bfbae6e2
CrazyMustard <simaosge6@hotmail.com>
2016-05-20 00:52:32 +0100

    Update players.c

--- players.c
+++ players.c
@@ -4,17 +4,21 @@
 
 const short CARD_POINTS[] = {0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
 
+/* FUNCTION NAME:GetPlayerListScore
+*  DESCRIPTION: Calculates the score of all players in the list */
 void GetPlayerListScore(Player_node *head) {
-    Player_node *walk = head;
+	Player_node *walk = head;
 
-    while (walk != NULL) {
-
-        GetScore(&(walk->player));
-
-        walk = walk->next;
-    }
+	/* Walks through all players in the list
+	and calculates their score*/
+	while (walk != NULL) {
+		GetScore(&(walk->player));
+		walk = walk->next;
+	}
 }
 
+/* FUNCTION NAME:GetScore
+*  DESCRIPTION: Calculates the player's score */
 void GetScore(Player* player) {
     short score = 0, aces = 0;
     Card_node *cardnode_ptr = NULL;
@@ -45,28 +49,24 @@ void GetScore(Player* player) {
     player->score = score;
 }
 
+/* FUNCTION NAME:add_player
+*  DESCRIPTION: Adds a player to the head of the players' list*/
 void add_player(Player_node** head, Player data) {
-    Player_node *new_node = NULL;
-
+	Player_node *new_node = NULL;
+	
+	/* Creates a node if possible, if not
+	exits the main program */
 	new_node = create_player_node(data);
 
-	if(empty(new_node)){
-		ALLOCATION_ERROR_MESSAGE();
-		exit(EXIT_FAILURE);
-	}
-
-    new_node->next = *head;
-    new_node->player = data;
-    *head = new_node;
+	/* Inserts a node in the head
+        with the specified data */
+	new_node->next = *head;
+	new_node->player = data;
+	*head = new_node;
 }
 
 void insert_player_node(Player_node **head, Player_node **tail, Player data) {
     Player_node *new_node = create_player_node();
-    
-    if (new_node == NULL) {
-        ALLOCATION_ERROR_MESSAGE();
-        exit(EXIT_FAILURE);
-    }
     
     if (*tail != NULL) {
         (*tail)->next = new_node;
@@ -82,70 +82,92 @@ void insert_player_node(Player_node **head, Player_node **tail, Player data) {
     }
 }
 
+/* FUNCTION NAME:take_player_node
+*  DESCRIPTION: Takes the node (if possible) from the selected 
+*  position of the list and returns a pointer to it*/
 Player_node *take_player_node(Player_node** head, int position) {
     Player_node *tmp = NULL;
     Player_node *curr = *head;
     
+    /* If the list isn't empty (not pointing to NULL) */
     if(!empty(curr)) {
+        /* There aren't negative positions on the list, so return without no actions done */
         if(position < 0)
             return NULL;
         
+        /* Takes a node from the first position of the list (head) */
         if(position == 0) {
             *head = curr->next;
             curr->next = NULL;
             return curr;
         }
         
+        /* Search the node in the position specified, until the end of the list */
         for(int i = 1; i < position && curr != NULL; i++)
             curr = curr->next;
         
+        /* If the position doesn't exist (more than the size of the list) 
+	returns with the list unchanged */
         if(empty(curr)) {
             return NULL;
         }
         
-        tmp = curr->next;
+        tmp = curr->next; //Gets the node to remove
         
+        /* If the node exists removes it from the list */
         if (tmp != NULL) {
             curr->next = curr->next->next;
             tmp->next = NULL;
         }
         
-        return tmp;
+        return tmp; //Returns pointer to the taken node
     }
     
     return NULL;
 }
 
+/* FUNCTION NAME:join_player_node
+*  DESCRIPTION: Joins a node (if possible) in the 
+selected position, else doesn't do anything*/
 void join_player_node(Player_node** head, Player_node* jointo, int position) {
     Player_node *curr = *head;
-    if (jointo != NULL) {
+   
+    if (!empty(jointo)) { //If there is a node
+    	/* If the list isn't empty (not pointing to NULL) */
         if(!empty(curr)) {
-            
+            /* There aren't negative positions on the list, so return without no actions done */
             if(position < 0) {
                 return;
             }
             
+            /* Joins a node in the first position of the list (head) */
             if(position == 0){
                 jointo->next = *head;
                 *head = jointo;
                 return;
             }
             
+            /* Search the node in the position specified, until the end of the list */
             for(int i = 1; i < position && curr != NULL; i++)
                 curr = curr->next;
             
+            /* If the position doesn't exist (more than the size of the list) 
+	    returns with the list unchanged */
             if(empty(curr)) {
                 return;
             }
             
+            /* Joins node to the list's specified position */
             jointo->next = curr->next;
             curr->next = jointo;
-        } else {
+        } else {//if the list is empty the node to join is in the head (independent of the position)
             *head = jointo;
         }
     }
 }
 
+/* FUNCTION NAME:insert_sorted_player_node
+*  DESCRIPTION: */
 void insert_sorted_player_node(Player_node **head, Player data) {
     Player_node *tmp = NULL;
     Player_node *curr = *head;
@@ -172,20 +194,27 @@ void insert_sorted_player_node(Player_node **head, Player data) {
         }
     }
 }
-
+/* FUNCTION NAME:create_player_node
+*  DESCRIPTION: Creates a node */
 Player_node *create_player_node() {
+	
+	/*Allocates memory for a new node and 
+	if it's not capable, exits the main program*/
 	Player_node *new_node = malloc(sizeof(Player_node));
-
 	if(empty(new_node)) {
-    	ALLOCATION_ERROR_MESSAGE();
-    	exit(EXIT_FAILURE);
+    		ALLOCATION_ERROR_MESSAGE();
+    		exit(EXIT_FAILURE);
     }
 
-    return new_node;
+    return new_node; //returns a pointer to the created node
 }
+
+/* FUNCTION NAME:erase_player_list
+*  DESCRIPTION: Erases a list */
 void erase_player_list(Player_node* head) {
 	Player_node *tmp;
-
+	
+	/*Removes each list's element*/
 	while((tmp = head) != NULL){
 		head = head->next;
 		free(tmp);
